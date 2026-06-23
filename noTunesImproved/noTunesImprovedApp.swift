453a9954d3e06c40e99bbb36ba6220ee4d88189a
b4iterdev <thaimeo200389@gmail.com>
2026-06-23 17:54:32 +0700

    Initial Commit

---
+++ noTunesImproved/noTunesImprovedApp.swift
@@ -0,0 +1,17 @@
+//
+//  noTunesImprovedApp.swift
+//  noTunesImproved
+//
+//  Created by Nguyen Minh Thai on 23/6/26.
+//
+
+import SwiftUI
+
+@main
+struct noTunesImprovedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
